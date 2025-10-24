de3069e3724d30cc14b33cdc479b1361fb459a3b
Chris Barnes <chrislloydbarnes@gmail.com>
2025-10-24 10:36:39 +0100

    Make `Key` public

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 #![doc=include_str!("../README.md")]
 mod errors;
-pub use errors::{Accumulator, Error, Failure, Result};
+pub use errors::{Accumulator, Error, Failure, Key, Result};
 pub mod synch;
 pub use synch::{Validate, ValidateContext};
 mod wrapper;
